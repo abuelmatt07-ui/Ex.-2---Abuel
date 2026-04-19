8d10b2a088cd7a2f476c53c2497f47f76f741bf3
MA <mattlyndonabuel@Matts-MacBook-Air.local>
2026-04-19 18:29:45 +0800

    Ver 1.1 - Read Mode

--- Exercise 2/Exercise2_ABUEL.py
+++ Exercise 2/Exercise2_ABUEL.py
@@ -1,10 +1,17 @@
 
-try:
-    with open("Exercise 2/Notes.txt", "w") as Note:
-        Note.write(input("Input Note: "))
 
-except:
-    print("Error")
+with open("Exercise 2/Notes.txt", "w") as Note:
+    Note.write(input("Input Note: "))
+
+
+try:
+    with open("Exercise 2/Notes.txt", "r") as Note:
+        readNote = Note.read()
+        print(f"\nNote:\n - {readNote}\n")
+
+except FileNotFoundError:
+    print("\n* Note does not Exist!\n")
+
 
 
 
