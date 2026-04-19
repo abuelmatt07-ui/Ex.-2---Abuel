52b051f71ca129e7d9f93c8c6bb1b3d19f9e1bb6
MA <mattlyndonabuel@Matts-MacBook-Air.local>
2026-04-19 19:20:47 +0800

    Ver 1.3 - Write & Append are now Functions & Implemented Userinput.

--- Exercise_2/Exercise2_ABUEL.py
+++ Exercise_2/Exercise2_ABUEL.py
@@ -5,27 +5,61 @@ def Read():
             print(f"\nNote:\n{readNote}")
 
     except FileNotFoundError:
-        print("\n* Note does not exist!\n")
+        print("\n* Create note first!\n")
 
-
-
-
-with open("Exercise_2/Notes.txt", "w") as Note:
-    Note.write(f" - {input("- Input Note: ")}\n")
-
-
-Read()
-
-
-try:
-    with open("Exercise_2/Notes.txt", "a") as Note:
+def Write():
+    with open("Exercise_2/Notes.txt", "w") as Note:
         Note.write(f" - {input("- Input Note: ")}\n")
 
-except FileNotFoundError:
-    print("\n* Note does not exist!\n")
+def Append():
+    try:
+        with open("Exercise_2/Notes.txt", "r") as Note:
+            pass
+          
+        with open("Exercise_2/Notes.txt", "a") as Note:
+            Note.write(f" - {input("- Input Note: ")}\n")
 
+    except FileNotFoundError:
+        print("\n* Create note first!\n")
 
-Read()
+choices = ["1", "2", "3", "4"]
 
+while True:
 
+    print(f"\n{' INotes ':-^18}")
+    print("1. Write new note.\n2. Write on note.\n3. Read note.\n4. Exit")
+
+    while True:
+        try:
+            userIn = input("\n- Choice: ")
+            if userIn != "":
+
+                if userIn in choices:
+                    break
+                else:
+                    print("* Not valid choice! (1, 2, 3, 4)")
+            
+            else:
+                print("* Please input choice! (1, 2, 3, 4)")
+                    
+
+        except:
+            print("* Input Error!")
+
+    match userIn:
+        
+        case "1":
+            Write()
+        
+        case "2":
+            Append()
+
+        case "3":
+            Read()
+
+        case "4":
+
+            print("- EXITING PROGRAM...")
+
+            break
 
